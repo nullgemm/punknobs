de50eb8b8af44e4c38f38a7840c63358959f50a7
nullgemm <nullgemm@mailbox.org>
2024-12-14 20:07:42 +0100

    add infer scripts

---
+++ make/scripts/infer_get.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# get in the right folder
+path="$(pwd)/$0"
+folder=$(dirname "$path")
+cd "$folder"/../.. || exit
+
+# get infer
+version="v1.1.0"
+
+cd res || exit
+curl -L https://github.com/facebook/infer/releases/download/"$version"/infer-linux64-"$version".tar.xz -o infer.tar.xz
+tar -xvJf infer.tar.xz
+mv infer-linux64-"$version" infer
